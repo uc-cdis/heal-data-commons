85cd8eadfb8fdfb03fd8e05af95ea098a1636d2a
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-10-17 13:17:04 -0500

    feat(homepagePlaceholders): removed unneeded code comment

--- src/lib/home/Components/Hero/Hero.test.tsx
+++ src/lib/home/Components/Hero/Hero.test.tsx
@@ -5,13 +5,7 @@ import Hero from './Hero'; // Adjust the import based on your file structure
 describe('Hero Component', () => {
   test('renders the Hero component with the correct heading', () => {
     render(<Hero />);
-
-    // Check if the component is in the document
     const heroElement = screen.getByTestId('hero');
     expect(heroElement).toBeInTheDocument();
-
-    // Check if the heading is displayed correctly
-    const headingElement = screen.getByRole('heading', { name: /Hero/i });
-    expect(headingElement).toBeInTheDocument();
   });
 });
