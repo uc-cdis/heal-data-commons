8e9a05a7a5d17ef2587fe8eddf14af320953dc52
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-10-17 10:28:28 -0500

    feat(homepagePlaceholders): Removed files changes as result of adding JEST, will attempt to install JEST via react testing library instead

--- src/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.next/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Optional setup file
-};
