6abb90a62d110e70912a68a26564ee23316e883f
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-10-29 14:26:33 -0500

    feat(healButton): removed useless comments

--- src/lib/Home/Components/HealButtonLink/HealButtonLink.tsx
+++ src/lib/Home/Components/HealButtonLink/HealButtonLink.tsx
@@ -3,18 +3,18 @@ import ArrowIcon from '../../Assets/Icons/Icon-Arrow.svg';
 import Image from 'next/image';
 
 interface ButtonLinkProps {
-  href: string; // Link destination
-  label: string; // Button text
+  href: string;
+  label: string;
 }
 
 const HealButtonLink: React.FC<ButtonLinkProps> = ({ href, label }) => {
   return (
     <a
-      href={href} // Prevent navigation if disabled
+      href={href}
       className={
         'heal-button-link select-none border-2 border-heal-magenta bg-white text-white p-[0.0625rem] py-[0.2rem] rounded inline-block'
       }
-      role="button" // Ensures screen readers treat it like a button
+      role="button"
     >
       <span className="bg-heal-magenta p-1 rounded  ">{label}</span>
       <span className="bg-white text-heal-magenta ml-1">
