5343e51ae9b0428a08e0a2d53b12dfa26d7d1660
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-11-04 11:36:11 -0600

    feat(healButtons): Added tailwind classes to handle hover and focus states

--- src/lib/Home/Components/HealButtonLink/HealButtonLink.tsx
+++ src/lib/Home/Components/HealButtonLink/HealButtonLink.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import ArrowIcon from '../../Assets/Icons/Icon-Arrow.svg';
-import Image from 'next/image';
+import React, { useState } from 'react';
+import ArrowIcon from '../../Assets/Icons/IconArrow';
 
 interface ButtonLinkProps {
   href: string;
   label: string;
 }
 
-const HealButtonLink: React.FC<ButtonLinkProps> = ({ href, label }) => {
+const HealButtonLink: React.FC<ButtonLinkProps> = ({
+  href = 'https://www.altavista.com',
+  label,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <a
       href={href}
-      className={
-        'heal-button-link select-none border-2 border-heal-magenta bg-white text-white p-[0.0625rem] py-[0.2rem] rounded inline-block'
-      }
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className={`
+        heal-button-link
+        focus:border-4
+        select-none border-2
+        group
+        hover:bg-heal-magenta
+        border-heal-magenta
+        bg-white
+        text-white
+        p-[0.0625rem]
+        py-[0.2rem]
+        rounded
+        inline-block`}
       role="button"
     >
-      <span className="bg-heal-magenta p-1 rounded  ">{label}</span>
-      <span className="bg-white text-heal-magenta ml-1">
-        <Image className="inline" src={ArrowIcon} alt="" />
+      <span
+        className={`
+          group-hover:bg-white
+          group-hover:text-heal-magenta
+          bg-heal-magenta
+          p-1 rounded
+        `}
+      >
+        {label}
+      </span>
+      <span className="bg-white group-hover:bg-heal-magenta group-hover:text-white  text-heal-magenta ml-1">
+        <ArrowIcon />
       </span>
     </a>
   );
