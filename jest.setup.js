173336a1a0dbccd34c53e7d3c433ce3fd1e7902c
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-11-19 11:38:26 -0600

    feat(heroComponent): made multiple single line comments global for setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,13 +1,13 @@
 // file: jest.setup.js
 
+/* eslint-disable react/display-name */
 /* eslint-disable @next/next/no-img-element */
 import '@testing-library/jest-dom';
-// Mock complex NextJS imports that JEST cannot handle well
-jest.mock('../../Assets/Icons/Icon-Arrow.svg', () => {
-  // eslint-disable-next-line react/display-name
+
+// Mock the complex Next imports that JEST does not handle well
+jest.mock('*.svg', () => {
   return (props) => <img {...props} alt={''} />;
 });
 jest.mock('next/image', () => {
-  // eslint-disable-next-line react/display-name
   return (props) => <img {...props} alt={''} />;
 });
