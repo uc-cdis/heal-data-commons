c7389543b419329e9ee53c2605fbf6c3f18d5476
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-11-19 11:27:34 -0600

    feat(heroComponent): Fixed failing unit tests with mocks defined in jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,13 @@
 // file: jest.setup.js
+
+/* eslint-disable @next/next/no-img-element */
 import '@testing-library/jest-dom';
-// Mock the SVG globally for all tests
-jest.mock('*.svg', () => '<svg></svg>');
+// Mock complex NextJS imports that JEST cannot handle well
+jest.mock('../../Assets/Icons/Icon-Arrow.svg', () => {
+  // eslint-disable-next-line react/display-name
+  return (props) => <img {...props} alt={''} />;
+});
+jest.mock('next/image', () => {
+  // eslint-disable-next-line react/display-name
+  return (props) => <img {...props} alt={''} />;
+});
