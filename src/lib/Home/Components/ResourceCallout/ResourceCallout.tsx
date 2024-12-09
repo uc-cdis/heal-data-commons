815c756a08380feedb870f9ecbdf4618b76a5f74
Jarvis Raymond <jarvisraymond@uchicago.edu>
2024-12-09 15:01:03 -0600

    feat(quickLinks): fixed styling issues

--- src/lib/Home/Components/ResourceCallout/ResourceCallout.tsx
+++ src/lib/Home/Components/ResourceCallout/ResourceCallout.tsx
@@ -17,10 +17,13 @@ const ResourceCallout: React.FC<ResourceCalloutProps> = ({
   ResourceCalloutData,
 }) => {
   return (
-    <div data-testid="resource-callout" className="rounded shadow text-left">
+    <div
+      data-testid="resource-callout"
+      className="rounded shadow text-left h-full bg-heal-light_purple"
+    >
       <div
-        data-testid="resource-callout"
-        className="bg-heal-magenta text-white h-15 rounded-t text-xl font-bold  p-4"
+        data-testid="resource-callout-header"
+        className="bg-heal-magenta text-white h-15 rounded-t text-xl font-bold p-4"
       >
         {ResourceCalloutData.title}
       </div>
@@ -28,17 +31,14 @@ const ResourceCallout: React.FC<ResourceCalloutProps> = ({
         <ul className="list-none">
           {ResourceCalloutData.links.map(
             (resourceLink: ResourceCalloutLink, i: number) => (
-              <li
-                className="flex items-center space-x-2 align-top pb-2"
-                key={i}
-              >
-                <span className="mr-1">
+              <li className="flex space-x-2 align-top pb-2" key={i}>
+                <span className="mr-1 pt-2">
                   <IconLinkArrow />
                 </span>
                 <a
                   href={resourceLink.href}
                   className="text-heal-blue underline
-                decoration-solid hover:text-heal-magenta"
+                    decoration-solid hover:text-heal-magenta"
                 >
                   {resourceLink.title}
                 </a>
