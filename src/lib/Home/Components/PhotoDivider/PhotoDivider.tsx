6197d981f928cb30b2ac982f1772a345fe38aafe
Mingfei Shao <mshao1@uchicago.edu>
2024-12-11 15:56:54 -0600

    add photo divider

--- src/lib/Home/Components/PhotoDivider/PhotoDivider.tsx
+++ src/lib/Home/Components/PhotoDivider/PhotoDivider.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
+import photoDivider1 from './../../Assets/Images/photo-divider-1.webp';
+import photoDivider2 from './../../Assets/Images/photo-divider-2.webp';
+import photoDivider3 from './../../Assets/Images/photo-divider-3.webp';
+import photoDivider4 from './../../Assets/Images/photo-divider-4.webp';
+import photoDivider5 from './../../Assets/Images/photo-divider-5.webp';
 
 const PhotoDivider: React.FC = () => {
   return (
-    <div data-testid="photo-divider" className="photo-divider text-center p-4">
-      <h1 className="text-2xl font-bold">Photo Divider</h1>
+    <div data-testid="photo-divider" className="photo-divider py-4">
+          <div className="flex justify-between gap-4">
+            <div className="w-1/3 md:w-1/4 lg:w-1/5">
+              <Image src={photoDivider1} alt="" className="w-full h-full object-cover" />
+            </div>
+            <div className="w-1/3 md:w-1/4 lg:w-1/5">
+              <Image src={photoDivider2} alt="" className="w-full h-full object-cover" />
+            </div>
+            <div className="w-1/3 md:w-1/4 lg:w-1/5">
+              <Image src={photoDivider3} alt="" className="w-full h-full object-cover" />
+            </div>
+            <div className="hidden w-1/4 md:block lg:w-1/5">
+              <Image src={photoDivider4} alt="" className="w-full h-full object-cover" />
+            </div>
+            <div className="hidden lg:block lg:w-1/5">
+              <Image src={photoDivider5} alt="" className="w-full h-full object-cover" />
+            </div>
+        </div>
     </div>
   );
 };
