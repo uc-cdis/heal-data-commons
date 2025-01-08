88724abbc07bb623da9036b0a133bafe2de8e961
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-01-08 12:39:26 -0600

    spike(storyBookAccessibilityCI): added tailwind styles, support for SVGR svg imports in Storybook, wrote component test for Content Spotlight

---
+++ src/lib/Home/Components/ContentSpotlight/ContentSpotlight.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, userEvent, within, spyOn, fireEvent } from '@storybook/test';
+
+import ContentSpotlight from './ContentSpotlight';
+
+const meta = {
+  title: 'HomePage/ContentSpotlight',
+  component: ContentSpotlight,
+} satisfies Meta<typeof ContentSpotlight>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// export const Default: Story = {};
+
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const contentSpotlight = canvas.getByTestId('content-spotlight');
+    const spotlightButton = canvas.getByRole('button');
+    let successfulButtonClick = false;
+    spotlightButton.addEventListener(
+      'click',
+      (e) => e.preventDefault(),
+      (successfulButtonClick = true),
+    );
+    expect(contentSpotlight).toBeInTheDocument();
+    expect(spotlightButton).toBeInTheDocument();
+    fireEvent.click(spotlightButton);
+    expect(successfulButtonClick).toBe(true);
+  },
+};
