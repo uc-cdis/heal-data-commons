509007e048d160e8e733121805b972a8ad21ec86
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-01-14 14:13:08 -0600

    spike(storyBookAccessibilityCI): Added storybook story for Quote Spotlight

---
+++ src/lib/Home/Components/QuoteSpotlight/QuoteSpotlight.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, within } from '@storybook/test';
+import { MantineProvider } from '@mantine/core';
+import QuoteSpotlight from './QuoteSpotlight';
+
+const meta = {
+  title: 'HomePage/QuoteSpotlight',
+  component: QuoteSpotlight,
+} satisfies Meta<typeof QuoteSpotlight>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+// More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const testIds = ['quote-spotlight', 'citation'];
+    testIds.forEach((id) => {
+      const currEle = canvas.getByTestId(id);
+      expect(currEle).toBeInTheDocument();
+    });
+    const citation = canvas.getByTestId('citation');
+    expect(citation).toBeInTheDocument();
+  },
+};
