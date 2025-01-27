a5cc650de54a9076d02e03cdfe9eb8aa8e22dc38
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-01-27 13:51:34 -0600

    feat(homepageHeader): rebased home and HealHeader for merge conflict

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { GetServerSideProps } from 'next';
 import Hero from '../lib/Home/Components/Hero/Hero';
 import CarouselBanner from '../lib/Home/Components/CarouselBanner/CarouselBanner';
 import QuickLinks from '../lib/Home/Components/QuickLinks/QuickLinks';
