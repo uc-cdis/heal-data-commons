c70f400e7ca163a2eaf486eadbfdc42bf7b832e8
Mingfei Shao <mshao1@uchicago.edu>
2025-02-04 11:01:35 -0600

    add changes for deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
-import {
-  LandingPage,
-  LandingPageGetStaticProps as getStaticProps,
-} from '@gen3/frontend';
+// import {
+//   LandingPage,
+//   LandingPageGetStaticProps as getStaticProps,
+// } from '@gen3/frontend';
 
-export default LandingPage;
-export { getStaticProps };
+// export default LandingPage;
+// export { getStaticProps };
+
+import HealLandingPage from './Home';
+export default HealLandingPage;
