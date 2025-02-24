a6532bfd6cd7110f8d8528dc61cc40e9d401cbc2
Jarvis Raymond <jarvisraymond@uchicago.edu>
2025-02-24 14:39:12 -0600

    feat(healPlatformPage): Initial commit - UI rendering

---
+++ src/lib/NewlyAvailableDatasets/CardedPageContent.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Link from 'next/link';
+import { Text } from '@mantine/core';
+import CardGrid, { CardGridItem } from './CardGrid';
+import MultiPartText, { MultiPartTextPart } from './MultiPartText';
+
+export interface CardedPageConfig {
+  readonly title: string;
+  // readonly introduction?: ReadonlyArray<MultiPartTextPart>;
+  readonly introduction?: { type: string; content: string }[];
+  readonly sections: {
+    title: string;
+    cards: { title: string; content: string; linkText: string; link: string }[];
+  }[];
+  /*   readonly sections: ReadonlyArray<{
+    title: string;
+    introduction: string;
+    cards: [];
+ */ // cards: ReadonlyArray<CardGridItem>;  }>;
+}
+
+const CardedPageContent = ({
+  title,
+  introduction,
+  sections,
+}: CardedPageConfig) => {
+  return (
+    <React.Fragment>
+      <div className="py-10 text-sm px-10">
+        <Link legacyBehavior href={'/'}>
+          Home&nbsp;
+        </Link>
+        {' > '}
+        <span className="text-heal-primary">&nbsp;{title}</span>
+      </div>
+      <div className="text-md pl-10">
+        <Text className="font-bold text-4xl text-gen3-coal font-montserrat pb-8">
+          {title}
+        </Text>
+        <div className="pl-5">
+          <MultiPartText parts={introduction} />
+          {(sections || []).map(({ title, introduction, cards }, i) => {
+            return (
+              <div key={i} className="mt-10">
+                <Text className="font-bold text-3xl text-gen3-coal font-montserrat pb-8">
+                  {title}
+                </Text>
+                <Text className="font-montserrat text-xl">{introduction}</Text>
+                <CardGrid content={cards} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default CardedPageContent;
