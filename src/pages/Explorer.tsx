0cabd521e5e563d5e3307cdf54a6171ab855d3a4
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-08-18 13:56:21 -0500

    feat(heal-explorer-style): Initial commit

--- src/pages/Explorer.tsx
+++ src/pages/Explorer.tsx
@@ -1,4 +1,4 @@
-import {
+/* import {
   ExplorerPage,
   ExplorerPageGetServerSideProps as getServerSideProps,
 } from '@gen3/frontend';
@@ -11,4 +11,68 @@ registerCustomExplorerDetailsPanels();
 
 export default ExplorerPage;
 
+export { getServerSideProps }; */
+
+/* import React, { JSX } from 'react';
+
+import { registerCohortTableCustomCellRenderers } from '@/lib/CohortBuilder/CustomCellRenderers';
+import { registerCustomExplorerDetailsPanels } from '@/lib/CohortBuilder/FileDetailsPanel';
+import {
+  ExplorerPage,
+  ExplorerPageProps,
+  ExplorerPageGetServerSideProps as getServerSideProps,
+} from '@gen3/frontend';
+import Layout from '@/lib/Layouts';
+
+registerCohortTableCustomCellRenderers();
+registerCustomExplorerDetailsPanels();
+
+const GreenExplorerPage = (props: ExplorerPageProps): JSX.Element => {
+  return (
+    <Layout>
+      <div style={{ backgroundColor: 'green' }}>
+        <ExplorerPage {...props} />
+      </div>
+    </Layout>
+  );
+};
+
+export default GreenExplorerPage;
+
+export { getServerSideProps }; */
+
+import React, { JSX } from 'react';
+import { CohortBuilder, type CohortBuilderProps } from '@gen3/frontend';
+import { ExplorerPageGetServerSideProps as getServerSideProps } from '@gen3/frontend';
+import { useRouter } from 'next/router';
+import Layout from '@/lib/Layouts';
+// import your own header/footer here
+
+const GreenExplorerPage = ({
+  explorerConfig,
+  tabsLayout,
+  sharedFiltersMap,
+}: CohortBuilderProps): JSX.Element => {
+  const { query } = useRouter();
+  const activeTab =
+    typeof query.activeTab === 'string'
+      ? query.activeTab
+      : query.activeTab?.[0];
+
+  return (
+    <Layout>
+      <div style={{ backgroundColor: 'green' }}>
+        {/* your header here */}
+        <CohortBuilder
+          explorerConfig={explorerConfig}
+          tabsLayout={tabsLayout}
+          sharedFiltersMap={sharedFiltersMap}
+        />
+        {/* your footer here */}
+      </div>
+    </Layout>
+  );
+};
+
+export default GreenExplorerPage;
 export { getServerSideProps };
