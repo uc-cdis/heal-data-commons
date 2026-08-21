8fcce883550c1bf5aeeb7f7593ad372480668461
Jarvis <113449836+jarvisraymond-uchicago@users.noreply.github.com>
2026-08-21 14:25:37 -0500

    Feat/heal explorer style (#80)
    * feat(heal-explorer-style): Initial commit
    
    * feat(heal-explorer-style): cleaned up code in Explorer.tsx
    
    * feat(heal-explorer-style): Added storybook to check accessibility, updated themeColors to match branding
    
    * feat(heal-explorer-style): Commented out a11y test on Explorer page
    
    * feat(heal-explorer-style): Commented out a11y test on Explorer page using test-runner.ts and title
    
    * feat(heal-explorer-style): Updated Explorer.stories.tsx

--- src/pages/Explorer.tsx
+++ src/pages/Explorer.tsx
@@ -1,14 +1,23 @@
-import {
-  ExplorerPage,
-  ExplorerPageGetServerSideProps as getServerSideProps,
-} from '@gen3/frontend';
+import React, { JSX } from 'react';
+import { CohortBuilder, type CohortBuilderProps } from '@gen3/frontend';
+import { ExplorerPageGetServerSideProps as getServerSideProps } from '@gen3/frontend';
+import Layout from '@/lib/Layouts';
 
-import { registerCohortTableCustomCellRenderers } from '@/lib/CohortBuilder/CustomCellRenderers';
-import { registerCustomExplorerDetailsPanels } from '@/lib/CohortBuilder/FileDetailsPanel';
-
-registerCohortTableCustomCellRenderers();
-registerCustomExplorerDetailsPanels();
+const ExplorerPage = ({
+  explorerConfig,
+  tabsLayout,
+  sharedFiltersMap,
+}: CohortBuilderProps): JSX.Element => {
+  return (
+    <Layout>
+      <CohortBuilder
+        explorerConfig={explorerConfig}
+        tabsLayout={tabsLayout}
+        sharedFiltersMap={sharedFiltersMap}
+      />
+    </Layout>
+  );
+};
 
 export default ExplorerPage;
-
 export { getServerSideProps };
