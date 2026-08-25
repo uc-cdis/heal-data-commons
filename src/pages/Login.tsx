2083553e29222d68eabc9120c5865a1b75442c12
Mingfei Shao <mshao1@uchicago.edu>
2026-08-25 11:04:34 -0500

    style login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,17 @@
-import {
-  LoginPage,
-  LoginPageGetServerSideProps as getServerSideProps,
-} from '@gen3/frontend';
-export default LoginPage;
+import React, { JSX } from 'react';
+import { LoginPanel, } from '@gen3/frontend';
+import { LoginPageGetServerSideProps as getServerSideProps } from '@gen3/frontend';
+import Layout from '@/lib/Layouts';
 
+const LoginPage = ({
+  loginConfig,
+}: any) => {
+  return (
+    <Layout>
+      <LoginPanel {...loginConfig} />
+    </Layout>
+  );
+};
+
+export default LoginPage;
 export { getServerSideProps };
