0700c2d725c94caf3c29dec013b30ebab8504b3b
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-09-03 16:32:33 -0500

    chore(updateCommonsFrontendApp): Got Discovery Page to render

---
+++ src/pages/Discovery/[studyId].tsx
@@ -0,0 +1,7 @@
+import { DiscoveryPage, DiscoveryPageGetServerSideProps as getServerSideProps } from '@gen3/frontend';
+import { registerDiscoveryCustomCellRenderers } from '@/lib/Discovery/CustomCellRenderers';
+
+registerDiscoveryCustomCellRenderers();
+
+export default DiscoveryPage;
+export { getServerSideProps };
