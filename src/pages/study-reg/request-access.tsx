7542d105714a1346b06130a7b8d66a306aa3fb55
Jarvis Raymond <jarvisraymond@uchicago.edu>
2026-09-16 11:50:43 -0500

    chore(updateCommonsFrontendAppImports): updated form imports

--- src/pages/study-reg/request-access.tsx
+++ src/pages/study-reg/request-access.tsx
@@ -14,7 +14,7 @@ const StudyRegistrationAccessRequestPage = ({ config }: Props) => {
   return (
     <Layout>
       <PageTitle pageName="Study Registration Access Request" />
-      <div className="flex justify-center items-start w-full">
+      <div className="w-full max-w-2xl mx-auto px-6">
         <GenericRegistrationAccessRequestForm config={config} />
       </div>
     </Layout>
